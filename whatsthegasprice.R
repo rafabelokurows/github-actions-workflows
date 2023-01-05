6cf8bd475bc81bc7529e0bfae9e3626ff81ab56c
Rafael Belokurows <rafabelokurows@gmail.com>
2023-01-05 18:36:17 +0000

    Create whatsthegasprice.R

---
+++ whatsthegasprice.R
@@ -0,0 +1,12 @@
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+raw_data <- httr::GET("https://www.fueleconomy.gov/ws/rest/fuelprices")
+
+api_char = rawToChar(raw_data$content)
+api_json = jsonlite::fromJSON(api_char,simplifyDataFrame = T)
+
+df = api_json %>% as.data.frame() %>% mutate(time=Sys.time(),.before = everything())
+
+write.csv(df,paste0("data/",Sys.Date(),"_fuel_prices.csv"),row.names = F)
